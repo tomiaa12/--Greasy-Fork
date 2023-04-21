3447a886aa9326f883ec24fce6102c48b2526292
邝永鑫 <kuangyongxin@iecas.cc>
2023-04-21 09:06:46 +0800

    fix: douyu 活动直播间白屏

--- douyu.js
+++ douyu.js
@@ -8,28 +8,25 @@
 // @match      *://www.douyu.com/*
 
 // ==/UserScript==
-;(() => {
-  const $ = id => document.querySelector(id);
+(() => {
+  const $ = (id) => document.querySelector(id);
   console.clear();
-  // setTimeout(() => {
-    
-  // }, 1000);
-  console.log(document.querySelector('.Header-download-wrap'));
   const config = {
-    'https://www.douyu.com/directory/myFollow': { // 我的关注
+    "https://www.douyu.com/directory/myFollow": {
+      // 我的关注
       remove: [
         /* 头部nav */
-        '.Header-download-wrap',   // 下载客户端
-        '.Header-broadcast-wrap',  // 开播
-        '.DropPane-ad',            // 我的关注
-        '.DropMenuList-ad',        // 分类
-        '.Video',                  // 视频
-        '.Game',                   // 游戏
-        '.Search-suggestBoxContent',// 搜索
+        ".Header-download-wrap", // 下载客户端
+        ".Header-broadcast-wrap", // 开播
+        ".DropPane-ad", // 我的关注
+        ".DropMenuList-ad", // 分类
+        ".Video", // 视频
+        ".Game", // 游戏
+        ".Search-suggestBoxContent", // 搜索
 
-        '.layout-Banner',          // banner
-        '.layout-Module-extra',    // 关注右上角
-        '.Prompt-link',
+        ".layout-Banner", // banner
+        ".layout-Module-extra", // 关注右上角
+        ".Prompt-link",
       ],
       css: `
       /* hover头像 */
@@ -39,52 +36,47 @@
       {
         display: none;
       }
-      `
+      `,
     },
-    'default': {
+    default: {
       remove: [
         /* 头部nav */
-        '.Header-download-wrap',   // 下载客户端
-        '.Header-broadcast-wrap',  // 开播
-        '.DropPane-ad',            // 我的关注
-        '.DropMenuList-ad',        // 分类
-        '.Video',                  // 视频
-        '.Game',                   // 游戏
-        '.Search-suggestBoxContent',// 搜索
-        '.ActBase',                // 右侧广告
-        '.VideoEntry-tab',         // 主播投稿
-        '.layout-Player-rank',     // 排行
-        '.Title-report',           // 举报
-        '.Title-category',         // 房间分类
-        '.BarrageSuspendedBallAd', // 弹幕列表弹窗广告
-        '.SysSign-Ad',             // 弹幕下方广告
-        '.layout-Bottom',          // 底部鱼吧
-        '.Title-anchorFriendWrapper',// 邻友
-        '.Title-sharkWeight',      // 重量
-        '.Title-row-span',         // 主播玩过
-        '.Title-ad',               // 视频右上广告
-        '.Title > .Title-roomInfo > div:last-child',
-        '.Title-anchorPic-bottom', // 头像下方
-        '.ToolbarActivityArea',    // 视频下方 任务鱼吧
-        '.PlayerToolbar-signCont',// 下方通知
-        '.TVGameEntry',            // 视频弹出
-        '.GuessGameMiniPanelB-wrapper',// 鱼丸预言
-        '.recommendAppBg-2dde55',// 下载app
-        /* 活动直播间 */
-        '.wm-view',
-        '#bc7',// 上面黄色区域
-        '#js-room-activity', // 右边 广告
-        '.adPicRoot_4kxGCX', // 视频左边广告
-        '#js-player-toolbar', // 下方送礼物
-        '#js-aside', // 左侧nav导航
-        '.watermark-442a18', // 视频左下角房间号
-        '.FansMedalDialog-normal', // 发弹幕徽章弹窗
-        '#bc173',
-        '.ActPayDialog',
-        '.Title-txAuthentication',
-        '#bc4',
-        '#bc217',
-        
+        ".Header-download-wrap", // 下载客户端
+        ".Header-broadcast-wrap", // 开播
+        // ".DropPane-ad", // 我的关注
+        ".DropMenuList-ad", // 分类
+        ".Video", // 视频
+        ".Game", // 游戏
+        ".Search-suggestBoxContent", // 搜索
+        ".ActBase", // 右侧广告
+        ".VideoEntry-tab", // 主播投稿
+        ".layout-Player-rank", // 排行
+        ".Title-report", // 举报
+        ".Title-category", // 房间分类
+        ".BarrageSuspendedBallAd", // 弹幕列表弹窗广告
+        ".SysSign-Ad", // 弹幕下方广告
+        ".layout-Bottom", // 底部鱼吧
+        ".Title-anchorFriendWrapper", // 邻友
+        ".Title-sharkWeight", // 重量
+        ".Title-row-span", // 主播玩过
+        ".Title-ad", // 视频右上广告
+        ".Title > .Title-roomInfo > div:last-child",
+        ".Title-anchorPic-bottom", // 头像下方
+        ".ToolbarActivityArea", // 视频下方 任务鱼吧
+        ".PlayerToolbar-signCont", // 下方通知
+        ".TVGameEntry", // 视频弹出
+        ".GuessGameMiniPanelB-wrapper", // 鱼丸预言
+        ".recommendAppBg-2dde55", // 下载app
+        // /* 活动直播间 */
+        ".wm-view",
+        "#js-room-activity", // 右边 广告
+        ".adPicRoot_4kxGCX", // 视频左边广告
+        "#js-player-toolbar", // 下方送礼物
+        "#js-aside", // 左侧nav导航
+        ".watermark-442a18", // 视频左下角房间号
+        ".FansMedalDialog-normal", // 发弹幕徽章弹窗
+        ".ActPayDialog",
+        ".Title-txAuthentication",
       ],
       css: `
       /* 橙色背景 */
@@ -141,54 +133,54 @@
         z-index: 1;
       }
       `,
-    }
-  }
-
+    },
+  };
 
   class BaiduClear {
-
-    constructor(config){
+    constructor(config) {
       this.config = config;
-      this.domain = '';
+      this.domain = "";
       this.query = null;
       this.init();
     }
 
-    init(){
-      const currentHref = location.href.split('?');
+    init() {
+      const currentHref = location.href.split("?");
       this.domain = currentHref[0];
       this.query = currentHref[1];
-      if(this.domain !== 'https://www.douyu.com/directory/myFollow')
-      this.domain = 'default';
-      console.log(this.domain);
+      if (this.domain !== "https://www.douyu.com/directory/myFollow")
+        this.domain = "default";
 
       this.removeToHidden(this.config?.[this.domain]?.remove);
       this.addCss(this.config?.[this.domain]?.css);
       this.remove(this.config?.[this.domain]?.remove);
       this.config?.[this.domain]?.fn?.();
-
     }
 
-    remove(arr = []){
-      arr.map(item => $(item)).forEach(item => {
-        item?.remove();
-      })
+    remove(arr = []) {
+      arr
+        .map((item) => $(item))
+        .forEach((item) => {
+          item?.remove();
+        });
     }
 
-    removeToHidden(arr = []){
-      if(!arr.length) return;
-      this.addCss(arr.join(',') + `{    
+    removeToHidden(arr = []) {
+      if (!arr.length) return;
+      this.addCss(
+        arr.join(",") +
+          `{    
         display: none !important;
-      }`)
+      }`
+      );
     }
 
-    addCss(css = ''){
-      if(!css) return;
-      let  style = document.createElement('style')
+    addCss(css = "") {
+      if (!css) return;
+      let style = document.createElement("style");
       style.innerHTML = css;
       document.documentElement.appendChild(style);
     }
-
   }
   new BaiduClear(config);
 })();
